a6e4733882cea866c78bd215bf95b7fca197ca92
romerachlin <rome.rachlin@post.runi.ac.il>
2024-01-03 14:56:11 +0200

    Update Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -1,13 +1,14 @@
 public class Perfect {
     public static void main(String[] args) {
         int input = Integer.parseInt(args[0]);
-        int sum = 0;
-        String divisorString = "";
+        int sum = 1;
+        String divisorString = "1";
 
-        for (int div = 1; div < input; div++) {
+        for (int div =2; div < input; div++) {
             if (input % div == 0) {
                 sum += div;
-                divisorString += div + (div != input - 1 ? " + " : "");
+               divisorString = divisorString+ " + "+ div ;
+			 
             }
         }
 
