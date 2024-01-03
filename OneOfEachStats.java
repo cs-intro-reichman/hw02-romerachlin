a125122b3eef596b179a4e450bec0ac9c4af2551
romerachlin <rome.rachlin@post.runi.ac.il>
2024-01-03 15:05:21 +0200

    Update OneOfEachStats.java

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -11,7 +11,7 @@ public class OneOfEachStats {
 	boolean girl = false;
 	int count = 0 ;
 	 do {
-	double RandomGenerator = new Random(seed); 
+	double RandomGenerator = new (Random(seed)); 
 	countC++ ;
 		if ((0.0 < RandomGenerator && RandomGenerator < 0.5)) {
 		boy = true ;
